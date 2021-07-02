66a1859a357f6e7fa248b1bd97048798857d9278
DigiKris <digikrisu@gmail.com>
2021-07-02 12:25:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CodeAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Starting variables 
+            int numberOne = 12932;
+            int numberTwo = -2828472;
+
+            // Use built-in methods and save to variable
+            /*double numberOneSqrt = Math.Floor(Math.Sqrt(numberOne));
+            double numberTwoSqrt = Math.Floor(Math.Sqrt(numberTwo));*/
+
+            Console.WriteLine(Math.Floor(Math.Sqrt(numberOne)));
+            Console.WriteLine(Math.Floor(Math.Sqrt(numberTwo)));
+        }
+    }
+}
